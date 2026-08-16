78e59459a4603870ac28d3936831b2b48fb00bb9
anishmanda986-arch <anishmanda986@gmail.com>
2026-08-16 19:06:40 +0530

    Fix Compose UI compilation

--- app/src/main/java/com/hireme/app/MainActivity.kt
+++ app/src/main/java/com/hireme/app/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
